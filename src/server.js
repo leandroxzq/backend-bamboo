0d06e9b5ba4372e6c47950a64a7ae364075ee40b
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-02-07 20:54:53 -0300

    Ajustando para usar a variável PORT do Heroku

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/routes.js';
+import express from "express"
+import cors from "cors"
+import routes from "./routes/routes.js"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-    })
-);
+	cors({
+		origin: "http://localhost:5173",
+	})
+)
 
-app.use('/', routes);
+app.use("/", routes)
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"))
 
-const port = 5000;
+const port = 5000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+app.listen(process.env.PORT || port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
